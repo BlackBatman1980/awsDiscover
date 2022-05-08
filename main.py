c32abbcb517e750684dad1131001c0e07a98ffd4
J0HN D03 <68918438+BlackBatman1980@users.noreply.github.com>
2022-05-08 22:07:26 +0000

    Insignificant update

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 import requests
 import statistics as stats
+from threading import Thread
 import sys
+import time
 import os
 from bs4 import BeautifulSoup
 
 totalKeys = 0
 
+
+wFoundUrls = []
 openFiles = []
 SC = []
 ET = []
 KEYS = []
 LM = []
 SIZE = []
+
+
+cmdlineHelp = f"[!] Please add website name and/or protocol\nEg. python3 {__file__} airbnb http\n=========================================\n      ADDITIONAL FLAGS\n-u    s3 Url Eg. airbnb\n-p    Protocol to use Eg. https http\n-a    Disable finding all accessible files\nEg2. python3 {__file__} -u airbnb -p http -a"
+
 
 help = '''
 clear -- clear screen
@@ -21,6 +29,8 @@ afs -- average file size
 info <filename> -- get info on a file
 af -- accessible files
 mk -- max keys
+======================================
+afscan -- scan for accessible files. include -t to run in background
 '''
 
 if os.name == "nt":
@@ -28,7 +38,7 @@ if os.name == "nt":
 else:
   clear = "clear"
 
-def usrShell():
+def usrShell(url):
   while True:
     usr = input("awsd> ")
     usr = usr.split()
@@ -36,22 +46,32 @@ def usrShell():
     if usr[0] == "help":
       print(help)
 
-    if usr[0] == "af":
+    elif usr[0] == "af":
       print("Total Accessible Files:",len(openFiles))
       print("File Listing:", openFiles)
 
-    if usr[0] == "exit":
+    elif usr[0] == "exit":
       exit()
 
-    if usr[0] == "mk":
+    elif usr[0] == "afscan":
+      if "-t" in usr:
+        print("[*] Running as a background job")
+        time.sleep(1)
+        thread = Thread(target=ACScanBG, args=[url])
+        thread.daemon = True
+        thread.start()
+      else:
+        ACScan(url)
+
+    elif usr[0] == "mk":
       maxKeys()
       
-    if usr[0] == "clear":
+    elif usr[0] == "clear":
       os.system(clear)
       
-    if usr == ["get","keys"]:
+    elif usr == ["get","keys"]:
       getFileNames()
-
+    
     elif usr[0] == "afs":
       averageFileSize()
 
@@ -71,18 +91,40 @@ def usrShell():
       except Exception as Error:
         print("[x] There was an error!\n{}".format(Error))
 try:
-  urlname = sys.argv[1]
-  proto = sys.argv[2]
+  if "-h" in sys.argv:
+    print(cmdlineHelp)
+    
+  else:
+    try:
+      index = (sys.argv).index("-u")
+      urlname = sys.argv[index + 1]
+    except ValueError:
+      print("[x] -u Flag not found")
+      print()
+      exit(1)
+
+  try:
+    index = (sys.argv).index("-p")
+    proto = sys.argv[index + 1]
+  except ValueError:
+    proto = "https"
+
 except:
-  print(f"[!] Please add website name and/or protocol\nEg. python3 {__file__} somewebsite http")
+  print(cmdlineHelp)
   exit(1)
+
+
+if "-a" in sys.argv:
+  isAC = False
+else:
+  isAC = True
 
   
 base = "-assets.s3.amazonaws.com"
 
 statusCodes = {403: "403 -- Forbidden", 404: "[x] 404 -- Not Found", 500: "500 -- Internal Server Error", 301: "301 -- Moved Permanently", 200: "[+] 200 -- OK"}
-
-def scan():
+    
+def defaultScan():
   url = proto + "://" + urlname + base
   print("[*] Scanning:", url)
   print()
@@ -111,12 +153,34 @@ def getFileNames():
   for name in KEYS:
     print(name)
 
+def ACScan(url):
+  complete = 0
+  for file in KEYS:
+    os.system(clear)
+    percent = round((complete / len(KEYS))*100, 2)
+    print("[*] Searching for accessible files...\n{}% Complete".format(percent))
+    url = url + "/" + file
+    r = requests.get(url)
+    if r.status_code == 200:
+      openFiles.append(file)
+    complete += 1
+
+def ACScanBG(url):
+  complete = 0
+  for file in KEYS:
+    url = url + "/" + file
+    r = requests.get(url)
+    if r.status_code == 200:
+      openFiles.append(file)
+    complete += 1
+  
+
 def enumURL(url):
   global totalKeys
   global soup
   global totalStorageClass
   
-  print("[*] Getting Data...", url)
+  print("[*] Getting Data...")
   content = requests.get(url).content
 
   soup = BeautifulSoup(content, features='xml')
@@ -135,19 +199,12 @@ def enumURL(url):
     LM.append(lastModified[i].get_text())
     SIZE.append(int(size[i].get_text()))
 
-  complete = 0
-  for file in KEYS:
+  if isAC:
+    ACScan(url)
     os.system(clear)
-    percent = round((complete / len(KEYS))*100, 2)
-    print("[*] Searching for accessible files...\n{}% Complete".format(percent))
-    url = url + "/" + file
-    r = requests.get(url)
-    if r.status_code == 200:
-      openFiles.append(file)
-    complete += 1
-
-  os.system(clear)
+    
   if totalStorageClass == totalKeys:
     print("[-] Total Contents Gathered:", totalKeys)
-  usrShell()  
-scan()
+  usrShell(url)
+
+defaultScan()
